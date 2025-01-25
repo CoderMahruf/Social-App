b8662c9ae65df7c3f9042c428bbc64aafa69e43d
mahruf60 <mahruf9060@gmail.com>
2025-01-26 01:12:21 +0600

    Logout functionality

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from .forms import LoginForm 
 from django.http import HttpResponse
 # Create your views here.
@@ -8,7 +8,7 @@ def user_login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            data = cleaned_data
+            data = form.cleaned_data
             user = authenticate(request,username=data['username'],password=data['password'])
             if user is not None:
                 login(request,user)
@@ -18,3 +18,7 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request,'users/login.html',{'form':form})
+
+def logout_view(request):
+    logout(request)
+    return render(request,'users/logout.html')
