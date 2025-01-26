4044030f75bb77af3e305851a79ce104b329d82c
mahruf60 <mahruf9060@gmail.com>
2025-01-26 17:15:31 +0600

    Password reset complete view

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('logout/',views.logout_view,name='logout'),
     path('password_change/',auth_view.PasswordChangeView.as_view(template_name='users/password-change-form.html'),name='password_change'),
     path('password_change/done/',auth_view.PasswordChangeDoneView.as_view(template_name='users/password-change-done.html'),name='password_change_done'),
-    path('password_reset/',auth_view.PasswordResetView.as_view(template_name='users/password-reset-form.html'),name='password_done'),
+    path('password_reset/',auth_view.PasswordResetView.as_view(template_name='users/password-reset-form.html'),name='password_reset'),
     path('password_reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='users/password-reset-done.html'),name='password_reset_done'),
     path('reset/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(template_name='users/password-reset-confirm.html'),name='password_reset_confirm'),
+    path('reset/done/',auth_view.PasswordResetCompleteView.as_view(template_name='users/password-reset-complete.html'),name='password_reset_complete'),
+
 ]
